5b6b78c1f114c1f9e0fe0676a48dd34d50bcc7d9
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-06-02 09:04:00 +0300

    tests(email): user email tests

---
+++ app/internal/core/domain/entity/user/user_email.go
@@ -0,0 +1,34 @@
+package entity
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/sanctumlabs/curtz/app/pkg/errdefs"
+)
+
+// Email is a domain entity for email
+type Email struct {
+	Value string
+}
+
+// NewEmail creates a new email
+func NewEmail(value string) (Email, error) {
+	if isValid(value) {
+		return Email{
+			Value: value,
+		}, nil
+	}
+	return Email{}, errdefs.ErrEmailInvalid
+}
+
+// isValid checks if an email address is valid
+func isValid(value string) bool {
+	pattern := `^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$`
+	re := regexp.MustCompile(pattern)
+	return re.Match([]byte(value))
+}
+
+func (e Email) format() string {
+	return strings.ToLower(e.Value)
+}
---
+++ app/internal/core/domain/entity/user/user_email_test.go
@@ -0,0 +1,59 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/sanctumlabs/curtz/app/pkg/errdefs"
+)
+
+type userEmailTestCase struct {
+	name  string
+	input string
+	err   error
+}
+
+var userEmailTestCases = []userEmailTestCase{
+	{
+		name:  "empty email should return nil and error",
+		input: "",
+		err:   errdefs.ErrEmailInvalid,
+	},
+	{
+		name:  "invalid email should return nil and error",
+		input: "johndoe",
+		err:   errdefs.ErrEmailInvalid,
+	},
+	{
+		name:  "invalid email should return nil and error",
+		input: "johndoe@",
+		err:   errdefs.ErrEmailInvalid,
+	},
+	{
+		name:  "valid email should return email and nil error",
+		input: "johndoe@example.com",
+		err:   nil,
+	},
+}
+
+func TestNewEmail(t *testing.T) {
+	for _, tc := range userEmailTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			email, err := NewEmail(tc.input)
+			if err != tc.err {
+				t.Errorf("NewEmail(%s) = (%v, %v), expected error %v, got %v", tc.input, email, err, tc.err, err)
+			}
+		})
+	}
+}
+
+func BenchmarkNewEmail(b *testing.B) {
+	if testing.Short() {
+		b.Skip("skipping benchmark in short mode")
+	}
+
+	for i := 0; i < b.N; i++ {
+		for _, tc := range userEmailTestCases {
+			NewEmail(tc.input)
+		}
+	}
+}
