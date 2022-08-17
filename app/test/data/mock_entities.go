e4ae6e1af75ae06458175e41796b8a7eb0c9e408
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-08-17 10:04:22 +0300

    style(lint): fix linting issues

--- app/test/data/mock_entities.go
+++ app/test/data/mock_entities.go
@@ -2,6 +2,7 @@ package data
 
 import "github.com/sanctumlabs/curtz/app/internal/core/entities"
 
+// MockUser creates a mock user given an email and a password
 func MockUser(email, password string) (entities.User, error) {
 	user, err := entities.NewUser(email, password)
 	return user, err
